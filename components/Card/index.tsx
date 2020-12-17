fe11e154c098efba29f7a23c60171a1c025e510d
Gui-dev <adrianon10@gmail.com>
2020-12-17 02:01:28 -0300

    style: add classes hover:bg-gray-600 transition

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -12,8 +12,8 @@ const Card: React.FC<CardProps> = ({ href, imgSrc, layoutId }) => {
   return (
     <Link href={ href }>
       <a>
-        <div className="relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer 
-          hover:bg-gray-100 transition">
+        <div className="relative h-20 md:h-40 bg-gray-700 rounded-xl cursor-pointer 
+          hover:bg-gray-600 transition">
           <motion.img src={ imgSrc } layoutId={ layoutId } className="w-full h-full"/>
         </div>
       </a>
